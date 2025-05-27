b9d2e1ca44b65276635a185911a400bf0ea530a1
TJK98 <wtj1998@naver.com>
2025-05-27 12:43:41 +0900

    JS-1-4-2-assigment

---
+++ 1-4-operater/2-assignment.js
@@ -0,0 +1,15 @@
+
+let number = 5;
+number += 3; //number = number + 3;
+//number =+ 3; //=은 항상 다른 연산자 오른쪽에
+number -= 6; //2
+number *= 5; //10
+number /= 2; //5
+number **= 3; //125
+number %= 10; //5
+
+console.log(`number: ${number}`);
+
+let num = 3;
+num++; //4 <- 연산 한번 적용 +1 할 때는 num++ 사용
+num += 1; //4 <- 연산 두번 적용 num = num + 1
