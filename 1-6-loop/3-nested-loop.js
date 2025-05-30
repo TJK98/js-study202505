d7ba4a9263f328e28277cbf56964b8dd5474b040
TJK98 <wtj1998@naver.com>
2025-05-30 19:32:08 +0900

    JS-1-6-3-nested-loop

---
+++ 1-6-loop/3-nested-loop.js
@@ -0,0 +1,17 @@
+for (let i = 0; i < 3; i++) {
+    console.log(`hello`);
+    for (let j = 0; j < 4; j++) {
+        console.log(`bye - (${i}, ${j})`);
+    }
+}
+
+
+/*
+for(let level = 2; level<=9; level++) {
+    console.log(`================ 구구단 ${level단} ================`);
+    for (let line = 1; line <= 9; line++) {
+        console.log(`* ${level} x ${line} = ${level * line}`);
+    }
+
+    console.log(`=============================================`);
+}*/
