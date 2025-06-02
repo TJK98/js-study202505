dca31a869a0cba3f8b23e39cde8988085aecc7f1
TJK98 <wtj1998@naver.com>
2025-06-02 15:53:35 +0900

    JS-2-1-3-practice, 1, 2, 3

---
+++ 2-1-array/practice1.js
@@ -0,0 +1,24 @@
+let numbers = [];
+let N = numbers.length;
+let inputMin = numbers[0];
+let inputMax = numbers[N - 1];
+
+for (let i = 0; i < 6; i++) {
+    numbers = +prompt(`
+숫자를 입력하세요.
+그만 하시려면 그만이라고 입력하세요.
+`);
+    if (number === `그만`) {
+        break;
+    }
+}
+
+/*
+if (N <= 5)
+    N++;
+alert(`
+입력한 숫자의 목록: ${numbers[N - 1]}
+입력한 숫자의 총합:
+`);
+
+if (N)*/
