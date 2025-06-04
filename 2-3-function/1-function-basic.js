1a64dce2cb33333119c259fd5e7c421fbc402842
TJK98 <wtj1998@naver.com>
2025-06-04 12:52:00 +0900

    JS-2-3-1-function-basic

---
+++ 2-3-function/1-function-basic.js
@@ -0,0 +1,20 @@
+// 함수 정의 : 함수를 만드는 과정 - 코드가 실행되지 않습니다.
+function sayHello(username) {
+    console.log(`${username}님 환영합니다.`);
+    console.log(`오늘도 즐거운 하루 되세요!`);
+}
+
+// 함수 호출 : 함수를 실행시키는 과정
+sayHello(`홍길동`);
+
+sayHello(`왕택준`);
+
+/*
+console.log(`김철수님 환영합니다.`);
+console.log(`오늘도 즐거운 하루 되세요!`);
+
+console.log(`홍길동님 환영합니다.`);
+console.log(`오늘도 즐거운 하루 되세요!`);
+
+console.log(`박영희님 환영합니다.`);
+console.log(`오늘도 즐거운 하루 되세요!`);*/
