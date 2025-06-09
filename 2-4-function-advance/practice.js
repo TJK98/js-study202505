e500790e36f71e58b090abe6078b68b9894d3d63
TJK98 <wtj1998@naver.com>
2025-06-09 10:38:00 +0900

    JS-2-4-practice 수정

--- 2-4-function-advance/practice.js
+++ 2-4-function-advance/practice.js
@@ -39,14 +39,35 @@ const userList = [
 
 // userList의 모든 요소가 조건을 만족하면 true, 하나라도 만족하지 않으면 false를 반환하는 every()함수를 구현하세요.
 
-function every(callback) {
+/*function every(callback) {
     for (const user of userList) {
         if (!callback(user)) {
             return false;
         }
     }
     return true;
+    }*/
+
+/**
+ * @param callback - 모두가 이 조건에 부합한지 확인할 함수
+ * @returns {boolean} - 하나라도 만족하지 않으면 false,
+ *                      모두 만족하면 true
+ */
+function every(callback) {
+    // 유저 배열을 순회한다.
+    for (const user of userList) {
+        // 콜백조건을 실행했을 때 결과가 false인 경우
+        if (!callback(user)) {
+            // 더 볼 필요 없이 false를 반환한다.
+            return false;
+        }
     }
+    // 반복문이 모두 끝났는데도 리턴이 안나왔다면
+    // 모든 요소가 조건에 만족하는 것이므로 최종적으로 true 반환
+    return true;
+}
+
+
 // 모든 사용자가 서울에 사는가?
 const result1 = every(function (user) {
     return user.address === '서울';
@@ -62,13 +83,28 @@ console.log(`=============================================`);
 
 // userList 중 하나라도 조건을 만족하면 true, 모두 만족하지 않으면 false를 반환하는 some()함수를 구현하세요.
 
-function some(callback) {
+/*function some(callback) {
     for (const user of userList) {
         if (callback(user))
             return true;
     }
     return false;
+}*/
+
+function some(callback) {
+    // 유저 배열을 순회한다.
+    for (const user of userList) {
+        // 콜백조건을 실행했을 때 결과가 하나라도 true인 경우
+        if (callback(user)) {
+            // 더 볼 필요 없이 true를 반환한다.
+            return true;
+        }
+    }
+    // 반복문이 모두 끝났는데도 리턴이 안나왔다면
+    // 모든 요소가 조건에 만족하는 것 없는것이므로 최종적으로 false 반환
+    return false;
 }
+
 
 // 이름에 '왕'이 포함된 사람이 있는가?
 const result3 = some(function (user) {
@@ -83,14 +119,19 @@ console.log(`=============================================`);
 
 // 조건을 만족하는 사용자가 하나도 없으면 true, 있으면 false를 반환하는 none() 함수를 구현하세요.
 
-function none(callback) {
+/*function none(callback) {
     for (const user of userList) {
         if (callback(user)) {
             return false;
         }
     }
     return true;
+}*/
+
+function none(callback) {
+    return !some(callback);
 }
+
 
 // '게임'이라는 취미를 가진 사용자가 없는가?
 const result5 = none(user => user.hobbys.includes('게임')); // true
